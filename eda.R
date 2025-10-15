02f84bad286f139b53bca32abda02af0029de1fc
DuranMendez <murpys@gmail.com>
2025-10-15 17:11:13 -0400

    eda cont

--- eda.R
+++ eda.R
@@ -2,6 +2,7 @@
 library(ggplot2)
 library(FactoMineR)
 library(factoextra)
+library(dplyr)
 
 # get dataset
 train_trans <- read.csv("ieee-fraud-detection/train_transaction.csv")
@@ -75,10 +76,13 @@ head(train_trans$P_emaildomain, 30)
 sum(is.na(train_trans$V1)) # 279287 missing
 sum(is.na(train_trans$V15)) # 76073 missing
 
+
+
 # merging training sets
 train_merged <- merge(train_trans, train_id, by = "TransactionID")
 
-# working to reduce engineered columns V
+## working to reduce engineered columns V
+## ------------------------------------------------------------------------
 v_cols <- paste0("V", 1:339) # creates a vector with the names
 
 # replaces missing values
@@ -86,24 +90,89 @@ for(col in v_cols){
 train_merged[[col]][is.na(train_merged[[col]])] <- mean(train_merged[[col]], na.rm = TRUE)
 }
 
-# removes constant columns
-v_cols <- v_cols[sapply(train_merged[, v_cols], function(x) sd(x, na.rm=TRUE) != 0)]
+# removes constant columns from vector
+v_cols <- v_cols[sapply(train_merged[, v_cols], function(x)
+  !is.na(sd(x, na.rm = TRUE)) && sd(x, na.rm = TRUE) != 0)]
 
-# Run PCA on selected numeric columns
-#pca_result <- PCA(train_trans[, v_cols], scale.unit = TRUE, graph = FALSE)
+# Identify columns that match V<number> and are NOT in v_cols
+drop_cols <- names(train_merged)[grepl("^V[0-9]+$", names(train_merged)) & 
+                                   !names(train_merged) %in% v_cols]
 
-#subset_scaled <- scale(train_trans[, v_cols])
+# Remove them from the dataset
+train_merged <- train_merged[, !names(train_merged) %in% drop_cols, drop = FALSE]
+
+## Run PCA on selected numeric columns
 pca_result <- prcomp(train_merged[, v_cols], center = TRUE, scale. = TRUE)
+summary(pca_result)
 
-# Visualize variance explained
-fviz_eig(pca_result, 10)
+# keeping the first 18 PCs explaining 72.18% of the variance
+train_pca <- pca_result$x[, 1:18, drop = FALSE]
+# convert to dataframe
+train_pca <- as.data.frame(train_pca)
 
-# Visualize individuals and variables
-fviz_pca_biplot(pca_result)
+# remove v colmuns from data
+train_merged <- train_merged[, !grepl("^V[0-9]+$", names(train_merged)), drop = FALSE]
+head(train_merged, 10)
+
+v_cols
+
+
+#----------------------------------
+check_na <- function(feature) {
+  n <- sum(is.na(train_merged[[feature]]))
+  return(n)  
+}
 
 
 
 
+## checking features in merged data
+## ----------------------------------------------------------------
+sum(is.na(train_merged$dist1))  # 144233 missing
+# dropping dist1
+train_merged <- train_merged %>%
+  select(-dist1)
 
-# merge training data
+# dist2
+sum(is.na(train_merged$dist2))  # 106640 missing
+# dropping dist2
+train_merged <- train_merged %>%
+  select(-dist2)
+
+# addr1
+sum(is.na(train_merged$addr1))  # 60447 missing
+
+# addr2
+sum(is.na(train_merged$addr2))   # 60447 missing
+
+# addr2
+sum(is.na(train_merged$addr2))   # 60447 missing
+
+
+# D columns
+# ------------------------------------------------
+sum(is.na(train_merged$D1))  # 106640 missing
+d_cols <- paste0("D", 1:15) # creates a vector with the names
+
+# D1 218 
+# D2 113117 
+# D3 115174 
+# D4 79465 
+# D5 111158 
+# D6 76860 
+# D7 108093 
+# D8 69307 
+# D9 69307 
+# D10 75001 
+# D11 144233 
+# D12 85324 
+# D13 82297 
+# D14 82068 
+# D15 75916 
+
+dcols_drop <- d_cols[-1] 
+
+train_merged <- train_merged[, !names(train_merged) %in% dcols_drop, drop = FALSE]
+
+ 
 
